fad5d5bda6dec77986b7a0897a2af30d7dd2a086
izzys <izzy.schall@gmail.com>
2015-10-29 17:37:36 +0200

    starting a RL

---
+++ @Agent/Agent.m
@@ -0,0 +1,27 @@
+classdef Agent
+
+    
+    properties
+        
+
+        
+
+    end
+    
+    methods
+        
+
+        function [agnt] = Agent(varargin)
+
+        end
+        
+        function [agnt] = Init(agnt)
+ 
+        end
+        
+        function [Env,a] = GetAction(Env,s,r)
+ 
+        end
+
+    end
+end
---
+++ @Environment/Environment.m
@@ -0,0 +1,30 @@
+classdef Environment
+
+    properties
+        
+
+        
+
+    end
+    
+    methods
+        
+
+        function [Env] = Environment(varargin)
+
+        end
+        
+        function [Env] = Init(Env)
+ 
+        end
+        
+        function [Env] = GetReward(Env,a,s)
+ 
+        end        
+
+        function [Env] = GetNextState(Env,a,s)
+ 
+        end    
+
+    end
+end
